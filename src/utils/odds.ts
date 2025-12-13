6949c2458e41b1aa91d5ad106f18805136bcf85e
Sasa Lotina <lotinasa@gmail.com>
2025-12-13 21:15:45 +0100

    Add type ChildMarket

--- src/utils/odds.ts
+++ src/utils/odds.ts
@@ -263,7 +263,7 @@ export const createChildMarkets: (
         const homeAwayOddsWithSpreadAdjusted = adjustSpreadOnChildOdds(homeAwayFormattedOdds);
 
         homeAwayOddsWithSpreadAdjusted.forEach((data) => {
-            let childMarket = {
+            let childMarket: ChildMarket = {
                 leagueId: Number(data.sportId),
                 typeId: Number(data.typeId),
                 type: MarketTypeMap[data.typeId as MarketType]?.key || '',
@@ -308,7 +308,7 @@ export const createChildMarkets: (
             const minOdds = leagueInfoByTypeId?.minOdds;
             const maxOdds = leagueInfoByTypeId?.maxOdds;
 
-            const childMarket = {
+            const childMarket: ChildMarket = {
                 leagueId: Number(data.sportId),
                 typeId: Number(data.typeId),
                 type: MarketTypeMap[data.typeId as MarketType]?.key || '',
