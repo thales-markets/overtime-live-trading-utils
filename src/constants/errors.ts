8836be764eebec43d8eccc837fd0905e138d5526
Gorshtak <sg31191@gmail.com>
2026-01-08 16:45:46 +0100

    removed unused constraints

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -8,3 +8,5 @@ export const ZERO_ODDS_AFTER_SPREAD_ADJUSTMENT = 'Bad odds after spread adjustme
 export const NO_MARKETS_FOR_LEAGUE_ID = 'No child markets for leagueID';
 export const LAST_POLLED_TOO_OLD = 'Last polled data is too old';
 export const REMOVE_MIN_MAX_ODDS = 'Removing market due to min/max odds constraints';
+export const GAME_NOT_LIVE = 'Blocking game because it is no longer live.';
+export const GAME_CLOCK_ERROR = 'Blocking game due to clock:';
